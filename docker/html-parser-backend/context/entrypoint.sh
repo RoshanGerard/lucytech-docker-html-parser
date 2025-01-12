48e50b14b1b5340432b0bfbd9dfbb080a3ff051a
Roshan G. Bolonna <srgbolonna@gmail.com>
2025-01-12 19:02:45 +0530

    fixed: chromedp hangs when running html parser service in docker container.

--- docker/html-parser-backend/context/entrypoint.sh
+++ docker/html-parser-backend/context/entrypoint.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+# Start Xvfb with the specified server arguments
+Xvfb :99 -screen 0 1024x768x24 &
+
+# Set DISPLAY environment variable
+export DISPLAY=:99
+
+# Start your application
 /app/main
