79e0bd87d11dff51bafb434af7d9dba2cda32430
Roshan G. Bolonna <srgbolonna@gmail.com>
2025-01-12 13:38:31 +0530

    fixed: build all script fail due to invalid path.

--- bin/build.sh
+++ bin/build.sh
@@ -6,5 +6,5 @@ DIST_ROOT=$(cd ../ && pwd)
 
 source "${DIST_ROOT}/.properties"
 
-source "${DIST_ROOT}/html-parser-backend/build.sh"
-source "${DIST_ROOT}/html-parser-frontend/build.sh"
+source "${DIST_ROOT}/docker/html-parser-backend/build.sh"
+source "${DIST_ROOT}/docker/html-parser-frontend/build.sh"
